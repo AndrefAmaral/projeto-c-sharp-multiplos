ff984cb5e858eba20daed345a4df8d3d0829ce6a
Andre Felipe <andrefelipe.pi2@gmail.com>
2023-02-28 22:09:54 -0300

    Estrutura condicional

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Multiplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Digite dois numeros:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            if (a % b == 0 || b % a == 0)
+            {
+                Console.WriteLine("Sao multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao multiplos");
+            }
+        }
+    }
+}
